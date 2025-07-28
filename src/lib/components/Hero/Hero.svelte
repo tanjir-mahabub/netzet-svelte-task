c89b0271e676403eb2396543d7eb1a4f22b6db8b
tanjir-mahabub <md.tanjir.mahabub@gmail.com>
2025-07-28 23:25:58 +0600

    fix: resolve Hero section spacing

--- src/lib/components/Hero/Hero.svelte
+++ src/lib/components/Hero/Hero.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <section
-	class="relative flex flex-col-reverse items-center justify-between gap-6 px-2 sm:px-6 md:flex-row md:py-[70px] xl:py-[30px]"
+	class="relative flex flex-col-reverse items-center justify-between gap-6 px-2 sm:px-6 md:flex-row md:py-[70px] xl:py-[44px]"
 >
 	<div class="z-10 max-w-[516px] flex-1 space-y-6 tracking-normal">
 		<HeroHeading />
@@ -14,9 +14,9 @@
 	</div>
 
 	<div
-		class="pointer-events-none relative flex xl:block h-full mt-8 w-full flex-1 md:absolute md:-top-12 md:right-10 md:mt-0 md:h-full md:w-1/2"
+		class="pointer-events-none relative flex xl:block h-full mt-8 w-full flex-1 md:absolute md:-top-12 md:right-[45px] md:mt-0 md:h-full md:w-1/2"
 	>
-		<div class="relative mx-auto min-h-[250px] w-full md:min-h-[673px] xl:w-[666px]">
+		<div class="relative mx-auto min-h-[426px] w-full md:min-h-[673px] xl:w-[666px]">
 			<img src="/phone.png" alt="App Preview" class="h-full w-full object-contain" />
 		</div>
 	</div>
